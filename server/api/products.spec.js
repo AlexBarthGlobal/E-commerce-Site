6807f4a8300ea8acabe7a35ae30d6f460a536c43
jponto <ponto.jake@gmail.com>
2021-03-02 14:46:32 -0500

    bug fix: product api route tests

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -12,10 +12,27 @@ describe('Product routes', () => {
   })
 
   describe('/api/products/', () => {
-    const newProduct = {}
+    const newProductOne = {
+      name: 'W201',
+      description:
+        'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+      picture: 'http://dummyimage.com/173x216.bmp/5fa2dd/ffffff',
+      price: 4.63,
+      quantity: 48
+    }
 
-    beforeEach(() => {
-      return Product.create(newProduct)
+    const newProductTwo = {
+      name: 'Grand Prix',
+      description:
+        'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.',
+      picture: 'http://dummyimage.com/201x148.bmp/cc0000/ffffff',
+      price: 47.21,
+      quantity: 62
+    }
+
+    beforeEach(async () => {
+      await Product.create(newProductOne)
+      await Product.create(newProductTwo)
     })
 
     it('GET /api/products', async () => {
@@ -23,25 +40,33 @@ describe('Product routes', () => {
         .get('/api/products')
         .expect(200)
       expect(res.body).to.be.an('array')
-      expect(res.body[0]).to.be.equal(newProduct)
-      expect(res.body[0].quantity).to.be.equal(newProduct.quantity)
+      expect(res.body.length).to.be.equal(2)
+      expect(res.body[0]).to.be.equal(newProductOne)
+      expect(res.body[0].quantity).to.be.equal(newProductOne.quantity)
     })
   })
 
   describe('/api/products/:id', () => {
-    const newProduct = {}
+    const newProductOne = {
+      name: 'W201',
+      description:
+        'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+      picture: 'http://dummyimage.com/173x216.bmp/5fa2dd/ffffff',
+      price: 4.63,
+      quantity: 48
+    }
 
     beforeEach(() => {
-      return Product.create(newProduct)
+      return Product.create(newProductOne)
     })
 
     it('GET /api/products', async () => {
       const res = await request(app)
-        .get('/api/products')
+        .get('/api/products/1')
         .expect(200)
       expect(res.body).to.be.an('object')
-      expect(res.body).to.be.equal(newProduct)
-      expect(res.body.quantity).to.be.equal(newProduct.quantity)
+      expect(res.body).to.be.equal(newProductOne)
+      expect(res.body.quantity).to.be.equal(newProductOne.quantity)
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
