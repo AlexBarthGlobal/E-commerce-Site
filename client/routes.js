adc1f55a83320b1a6495404edbe0c7364fa2ab8e
Alex Barth <alexNYCbarth@gmail.com>
2021-03-03 12:11:40 -0500

    Fixed navbar & routes

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {withRouter, Route, Switch} from 'react-router-dom'
+import {withRouter, Route, Switch, Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome, AllProducts, SingleProduct} from './components'
 import {me} from './store'
@@ -19,11 +19,12 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/all-products" component={AllProducts} />
         <Route
-          path="/:productId"
+          path="/product/:productId"
           render={routeProps => <SingleProduct {...routeProps} />}
         />
         {isLoggedIn && (
